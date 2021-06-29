b4f5b7885e34f80e03898ef5bb6b1f26f94097a9
gudwh14 <alxious@naver.com>
2021-06-29 18:22:51 +0900

    feat : 서버로 부터 Cookie 받아오기 (httpOnly : false)
    httpOnly False 인 쿠키를 서버로부터 받아옴
    그러나 XSS 에 취약 하기 때문에 httpOnly true일때 쿠키를 받아와야함
    How...?

--- src/Cookie/Cookie.js
+++ src/Cookie/Cookie.js
@@ -9,6 +9,7 @@ const Cookie = () => {
 
 
     /*
+        Cookie 를 받아오기 위해서 withCredentials : true 를 설정 해준다.
         httpOnly = false 일때 document.cookie 로 받은 쿠키를 읽어올수 있다.
         But, httpOnly 이면 document.cookie JS 로 쿠키를 읽을수있어 XSS 공격에 취약하다.
      */
@@ -22,7 +23,6 @@ const Cookie = () => {
                 console.log(error);
             })
     }
-
     /*
         httpOnly = true 일때
         headers Set-Cookie 를  어떻게 get 해야할까..?
